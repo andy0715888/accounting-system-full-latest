ba7cb3e168ed5d63f5c84f5edb99c4800a43e292
andy0715888 <andy0715888@users.noreply.github.com>
2026-07-31 08:36:04 +0000

    feat: 添加系统加载旋转动画
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -714,53 +714,6 @@ function startHttp() {
                         keepaliveInterval: 15000,
                         keepaliveCountMax: 3,
                         tryKeyboard: true,
-                        algorithms: {
-                            kex: [
-                                'curve25519-sha256',
-                                'curve25519-sha256@libssh.org',
-                                'ecdh-sha2-nistp256',
-                                'ecdh-sha2-nistp384',
-                                'ecdh-sha2-nistp521',
-                                'diffie-hellman-group-exchange-sha256',
-                                'diffie-hellman-group14-sha256',
-                                'diffie-hellman-group15-sha512',
-                                'diffie-hellman-group16-sha512',
-                                'diffie-hellman-group14-sha1',
-                                'diffie-hellman-group1-sha1'
-                            ],
-                            cipher: [
-                                'aes128-ctr',
-                                'aes192-ctr',
-                                'aes256-ctr',
-                                'aes128-gcm',
-                                'aes128-gcm@openssh.com',
-                                'aes256-gcm',
-                                'aes256-gcm@openssh.com',
-                                'aes256-cbc',
-                                'aes192-cbc',
-                                'aes128-cbc',
-                                '3des-cbc',
-                                'blowfish-cbc'
-                            ],
-                            serverHostKey: [
-                                'ssh-ed25519',
-                                'ecdsa-sha2-nistp256',
-                                'ecdsa-sha2-nistp384',
-                                'ecdsa-sha2-nistp521',
-                                'rsa-sha2-512',
-                                'rsa-sha2-256',
-                                'ssh-rsa',
-                                'ssh-dss'
-                            ],
-                            hmac: [
-                                'hmac-sha2-256',
-                                'hmac-sha2-512',
-                                'hmac-sha1',
-                                'hmac-sha1-96',
-                                'hmac-md5',
-                                'hmac-md5-96'
-                            ]
-                        },
                         debug: (msg) => {
                             console.log(`[SSH debug ${host}:${port || 22}]`, msg);
                         }
